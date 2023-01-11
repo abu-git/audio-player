d1b391b8a7cbba61138be4e6e5b68877229f9cc6
Samuel Abu <abu.sammie@gmail.com>
2023-01-11 21:35:43 +0200

    audio player progress

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import styles from '../styles/AudioPlayer.module.css'
 
 const AudioPlayer = () => {
   return (
-    <div>
-        Audio Player
+    <div className={styles.audioPlayer}>
+        <audio src="/audio/backtalk.mp3" preload='metadata'></audio>
+        <button>back 30</button>
+        <button>play / pause</button>
+        <button>forward 30</button>
+
+        {/* current time */}
+        <div>0:00</div>
+
+        {/* progress bar */}
+        <div>
+            <input type='range' />
+        </div>
+
+        {/* duration */}
+        <div>2:49</div>
     </div>
   )
 }
 
-export default AudioPlayer
+export { AudioPlayer }
 
 
 
